46474990c6db6e0211a08b26ed85892ac0052d8a
Aki Ren Yu <71982165+akirenyu@users.noreply.github.com>
2024-11-09 01:31:00 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+// Navbar
+const navbar = document.getElementById("navbar");
+window.onscroll = function () {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
 }
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+
+// Atur Padding Main agar tidak tertutupi oleh Header
+window.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector(".navbar");
+    const main = document.querySelector(".main");
+    main.style.paddingTop = `${navbar.offsetHeight}px`;
+});
